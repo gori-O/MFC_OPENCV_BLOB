ec3e1459879d97c1bb05ea7e207cdf0469b1961e
gori-O <fheld1545@naver.com>
2023-12-15 16:08:49 +0900

    first commit

---
+++ INTEK_TEST1Dlg.cpp
@@ -0,0 +1,277 @@
+﻿
+// INTEK_TEST1Dlg.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "INTEK_TEST1.h"
+#include "INTEK_TEST1Dlg.h"
+#include "afxdialogex.h"
+#include <windows.h>
+#include <string>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// 응용 프로그램 정보에 사용되는 CAboutDlg 대화 상자입니다.
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+// 구현입니다.
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CINTEKTEST1Dlg 대화 상자
+
+
+
+CINTEKTEST1Dlg::CINTEKTEST1Dlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_INTEK_TEST1_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CINTEKTEST1Dlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_PIC, m_Pic);
+	DDX_Control(pDX, IDC_SLIDER1, m_Slider);
+	DDX_Control(pDX, IDC_EDIT1, m_edit_control1);
+}
+
+BEGIN_MESSAGE_MAP(CINTEKTEST1Dlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_NOTIFY(NM_CUSTOMDRAW, IDC_SLIDER1, &CINTEKTEST1Dlg::OnCustomdrawSlider1)
+	ON_BN_CLICKED(IDC_BUTTON1, &CINTEKTEST1Dlg::OnBnClickedButton1)
+	ON_BN_CLICKED(IDC_BUTTON2, &CINTEKTEST1Dlg::OnBnClickedButton2)
+END_MESSAGE_MAP()
+
+
+// CINTEKTEST1Dlg 메시지 처리기
+
+BOOL CINTEKTEST1Dlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 시스템 메뉴에 "정보..." 메뉴 항목을 추가합니다.
+	m_Slider.SetRange(0, 255);
+	m_Slider.SetPos(200);
+	m_threshold = 200;
+	m_pBitmapInfo = NULL;
+
+	// IDM_ABOUTBOX는 시스템 명령 범위에 있어야 합니다.
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != nullptr)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// 이 대화 상자의 아이콘을 설정합니다.  응용 프로그램의 주 창이 대화 상자가 아닐 경우에는
+	//  프레임워크가 이 작업을 자동으로 수행합니다.
+	SetIcon(m_hIcon, TRUE);			// 큰 아이콘을 설정합니다.
+	SetIcon(m_hIcon, FALSE);		// 작은 아이콘을 설정합니다.
+
+	// TODO: 여기에 추가 초기화 작업을 추가합니다.
+
+	return TRUE;  // 포커스를 컨트롤에 설정하지 않으면 TRUE를 반환합니다.
+}
+
+void CINTEKTEST1Dlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// 대화 상자에 최소화 단추를 추가할 경우 아이콘을 그리려면
+//  아래 코드가 필요합니다.  문서/뷰 모델을 사용하는 MFC 애플리케이션의 경우에는
+//  프레임워크에서 이 작업을 자동으로 수행합니다.
+
+void CINTEKTEST1Dlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 클라이언트 사각형에서 아이콘을 가운데에 맞춥니다.
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 아이콘을 그립니다.
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+// 사용자가 최소화된 창을 끄는 동안에 커서가 표시되도록 시스템에서
+//  이 함수를 호출합니다.
+HCURSOR CINTEKTEST1Dlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+
+void CINTEKTEST1Dlg::OnCustomdrawSlider1(NMHDR* pNMHDR, LRESULT* pResult)
+{
+	LPNMCUSTOMDRAW pNMCD = reinterpret_cast<LPNMCUSTOMDRAW>(pNMHDR);
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	int slider_pos;
+	slider_pos = m_Slider.GetPos();
+	m_threshold = slider_pos;
+	SetDlgItemInt(IDC_EDIT1, slider_pos);
+
+	*pResult = 0;
+}
+
+
+void CINTEKTEST1Dlg::OnBnClickedButton1()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	// "load image" 버튼클릭이벤트
+	CFileDialog fileDlg(TRUE, NULL, NULL, OFN_READONLY, _T("image file(*.jpg;*.bmp;*.png;)|*.jpg;*.bmp;*.png;|All Files(*.*)|*.*||"));
+	if (fileDlg.DoModal() == IDOK)
+	{
+		CString path = fileDlg.GetPathName();
+
+		CT2CA pszString(path);
+		std::string strPath(pszString);
+
+		src_img = imread(strPath, IMREAD_UNCHANGED);
+
+		CreateBitmapInfo(src_img, src_img.cols, src_img.rows, src_img.channels() * 8);
+		DrawImage(src_img);
+	}
+}
+
+void CINTEKTEST1Dlg::OnBnClickedButton2()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	// "blob 연산" 버튼클릭이벤트 
+	img_gray = src_img.clone();
+	src_clone_img = src_img.clone();
+	cvtColor(src_img, img_gray, COLOR_BGR2GRAY);
+
+	threshold(img_gray, img_threshold, m_threshold, 255, THRESH_BINARY_INV);
+
+	Mat img_labels, stats, centroids;
+	int numOfLables = connectedComponentsWithStats(img_threshold, img_labels, stats, centroids, 8, CV_32S);
+
+	for (int j = 1; j < numOfLables; j++) {
+		int area = stats.at<int>(j, CC_STAT_AREA);
+		int left = stats.at<int>(j, CC_STAT_LEFT);
+		int top = stats.at<int>(j, CC_STAT_TOP);
+		int width = stats.at<int>(j, CC_STAT_WIDTH);
+		int height = stats.at<int>(j, CC_STAT_HEIGHT);
+
+		rectangle(src_clone_img, Point(left, top), Point(left + width, top + height),
+			Scalar(0, 0, 255), 1);
+	}
+
+	CreateBitmapInfo(src_clone_img, src_clone_img.cols, src_clone_img.rows, src_clone_img.channels() * 8);
+	DrawImage(src_clone_img);
+}
+
+
+void CINTEKTEST1Dlg::CreateBitmapInfo(Mat& img, int w, int h, int bpp)
+{
+	if (m_pBitmapInfo != NULL)
+	{
+		delete m_pBitmapInfo;
+		m_pBitmapInfo = NULL;
+	}
+
+	if (bpp == 8)
+		m_pBitmapInfo = (BITMAPINFO*) new BYTE[sizeof(BITMAPINFO) + 255 * sizeof(RGBQUAD)];
+	else // 24 or 32bit
+		m_pBitmapInfo = (BITMAPINFO*) new BYTE[sizeof(BITMAPINFO)];
+
+	m_pBitmapInfo->bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+	m_pBitmapInfo->bmiHeader.biPlanes = 1;
+	m_pBitmapInfo->bmiHeader.biBitCount = bpp;
+	m_pBitmapInfo->bmiHeader.biCompression = BI_RGB;
+	m_pBitmapInfo->bmiHeader.biSizeImage = 0;
+	m_pBitmapInfo->bmiHeader.biXPelsPerMeter = 0;
+	m_pBitmapInfo->bmiHeader.biYPelsPerMeter = 0;
+	m_pBitmapInfo->bmiHeader.biClrUsed = 0;
+	m_pBitmapInfo->bmiHeader.biClrImportant = 0;
+
+	if (bpp == 8)
+	{
+		for (int i = 0; i < 256; i++)
+		{
+			m_pBitmapInfo->bmiColors[i].rgbBlue = (BYTE)i;
+			m_pBitmapInfo->bmiColors[i].rgbGreen = (BYTE)i;
+			m_pBitmapInfo->bmiColors[i].rgbRed = (BYTE)i;
+			m_pBitmapInfo->bmiColors[i].rgbReserved = 0;
+		}
+	}
+
+	m_pBitmapInfo->bmiHeader.biWidth = w;
+	m_pBitmapInfo->bmiHeader.biHeight = -h;
+}
+void CINTEKTEST1Dlg::DrawImage(Mat& img)
+{
+	CClientDC dc(GetDlgItem(IDC_PIC));
+
+	CRect rect;
+	GetDlgItem(IDC_PIC)->GetClientRect(&rect);
+
+	SetStretchBltMode(dc.GetSafeHdc(), COLORONCOLOR);
+	StretchDIBits(dc.GetSafeHdc(), 0, 0, rect.Width(), rect.Height(), 0, 0, img.cols, img.rows, img.data, m_pBitmapInfo, DIB_RGB_COLORS, SRCCOPY);
+}
---
+++ INTEK_TEST1Dlg.h
@@ -0,0 +1,49 @@
+﻿
+// INTEK_TEST1Dlg.h: 헤더 파일
+//
+
+#pragma once
+#include <opencv2/opencv.hpp>
+using namespace cv;
+
+// CINTEKTEST1Dlg 대화 상자
+class CINTEKTEST1Dlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CINTEKTEST1Dlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+public:
+	int m_threshold;
+	Mat src_img, src_clone_img, img_gray, img_threshold;
+	BITMAPINFO* m_pBitmapInfo;
+
+	void CreateBitmapInfo(Mat& img, int w, int h, int bpp);
+	void DrawImage(Mat& img);
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_INTEK_TEST1_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CStatic m_Pic;
+	CSliderCtrl m_Slider;
+	afx_msg void OnCustomdrawSlider1(NMHDR* pNMHDR, LRESULT* pResult);
+	afx_msg void OnBnClickedButton1();
+	afx_msg void OnBnClickedButton2();
+	CEdit m_edit_control1;
+};
